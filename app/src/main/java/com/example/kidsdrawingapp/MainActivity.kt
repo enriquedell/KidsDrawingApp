e54d883d7374c236d9b18c856dc2cc2ba7f1828f
enriquedell <enriquedell@gmail.com>
2026-09-19 07:53:18 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,1 +1,38 @@
-// MainActivity file created
+package com.example.kidsdrawingapp
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawingView: DrawingView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawingView = findViewById(R.id.drawingView)
+
+        val clearButton: Button = findViewById(R.id.clearButton)
+        clearButton.setOnClickListener {
+            drawingView.clearCanvas()
+        }
+
+        val redButton: Button = findViewById(R.id.redButton)
+        redButton.setOnClickListener {
+            drawingView.setColor(Color.RED)
+        }
+
+        val blueButton: Button = findViewById(R.id.blueButton)
+        blueButton.setOnClickListener {
+            drawingView.setColor(Color.BLUE)
+        }
+
+        val blackButton: Button = findViewById(R.id.blackButton)
+        blackButton.setOnClickListener {
+            drawingView.setColor(Color.BLACK)
+        }
+    }
+}
